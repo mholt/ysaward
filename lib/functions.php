cfba5f3938f618b81d02c9466a93b46da3317088
Matthew Holt <Matthew.Holt+git@gmail.com>
2013-11-07 22:31:42 -0700

    Fixed failover handler (oops)

--- lib/functions.php
+++ lib/functions.php
@@ -55,12 +55,11 @@ function fail($msg)
 
 function euroToUsdRate()
 {
-	$result = json_decode(file_get_contents("http://rate-exchange.appspot.com/currency?from=EUR&to=USD"));
-	if (!$result || !$result->rate)
-	{
-		$result = new stdClass;
-		$result->rate = 1.3;	// fairly safe default in case request fails
-	}
+	$raw = @file_get_contents("http://rate-exchange.appspot.com/currency?from=EUR&to=USD");
+	$result = new stdClass;
+	$result->rate = 1.3;	// fairly safe default in case request fails
+	if ($raw)
+		$result = json_decode($raw);
 	return $result->rate;
 }
 
