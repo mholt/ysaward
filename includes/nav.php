97b4fc26aeebc451679fd2e45cd4ce6bf59442e6
Matthew Holt <Matthew.Holt+git@gmail.com>
2014-06-05 17:48:48 -0600

    Updated icons in nav

--- includes/nav.php
+++ includes/nav.php
@@ -1,20 +1,20 @@
 <nav>
 <?php if ($MEMBER != null && $LEADER == null): ?>
 	<b>Me</b>
-	<a href="/profile"><i class="icon-pencil"></i>Edit profile</a>
-	<a href="/survey"><i class="icon-edit" style="margin-right: .45em;"></i>Edit survey</a>
+	<a href="/profile"><i class="fa fa-pencil"></i>Edit profile</a>
+	<a href="/survey"><i class="fa fa-edit" style="margin-right: .45em;"></i>Edit survey</a>
 
 	<b>Membership</b>
-	<a href="/directory"><i class="icon-list-alt"></i>Directory</a>
+	<a href="/directory"><i class="fa fa-list-alt"></i>Directory</a>
 	<?php if ($MEMBER && $MEMBER->HasPresetCalling()): ?>
-	<a href="/exportmls"><i class="icon-share"></i>Export to MLS</a>
+	<a href="/exportmls"><i class="fa fa-share"></i>Export to MLS</a>
 	<?php endif; ?>
-	<a href="/fhe"><i class="icon-group"></i>FHE groups</a>
-	<a href="/callings"><i class="icon-sitemap"></i>Callings</a>
+	<a href="/fhe"><i class="fa fa-group"></i>FHE groups</a>
+	<a href="/callings"><i class="fa fa-sitemap"></i>Callings</a>
 
 	<b>Send</b>
-	<a href="/sms"><i class="icon-comments-alt"></i>Texts</a>
-	<a href="/email"><i class="icon-envelope"></i>Emails</a>
+	<a href="/sms"><i class="fa fa-comments"></i>Texts</a>
+	<a href="/email"><i class="fa fa-envelope"></i>Emails</a>
 
 	<!--
 	<b>LDS.org</b>
@@ -43,15 +43,15 @@
 	$wardsQuery = DB::Run("SELECT Name, ID FROM Wards WHERE StakeID='{$LEADER->StakeID}' AND Deleted != 1 ORDER BY Name ASC");
 	while ($wardRow = mysql_fetch_array($wardsQuery)):
 	?>
-		<a href="/api/changeward?id=<?php echo $wardRow['ID']; ?>"><i class="icon-asterisk"></i><?php echo $wardRow['Name']; ?></a></li>
+		<a href="/api/changeward?id=<?php echo $wardRow['ID']; ?>"><i class="fa fa-asterisk"></i><?php echo $wardRow['Name']; ?></a></li>
 	<?php endwhile; ?>
 
 	<b>Membership</b>
-	<a href="/directory"><i class="icon-list-alt"></i>Directory</a>
+	<a href="/directory"><i class="fa fa-list-alt"></i>Directory</a>
 <?php endif; ?>
 	
 	<br>
-	<a href="/logout"><i class="icon-signout"></i>Log out</a>
+	<a href="/logout"><i class="fa fa-sign-out"></i>Log out</a>
 
 
 </nav>
